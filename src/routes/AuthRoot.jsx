2961ac804582567f95bee120956f21bf07f6cb5a
lauravivan <lauvivang@gmail.com>
2024-02-27 11:03:07 -0300

    Adjustments on mobile screen

--- src/routes/AuthRoot.jsx
+++ src/routes/AuthRoot.jsx
@@ -10,16 +10,14 @@ function AuthRoot() {
   }
 
   return (
-    <main className="h-screen flex">
-      <div className="bg-light flex flex-1">
-        <div className="sm:min-w-[28rem] md:min-w-[35rem] py-8 px-4 flex-1 sm:flex-none m-auto bg-dark text-white rounded transition-spacing transition-flex ease-in-out duration-1000 sm:px-8 lg:w-2/5">
-          <section className="relative">
-            <article>
-              <Outlet />
-            </article>
-            <Cat className="w-20 absolute -top-16 right-0" />
-          </section>
-        </div>
+    <main className="h-screen flex bg-light">
+      <div className="sm:min-w-[28rem] md:min-w-[35rem] py-8 px-4 flex items-center flex-1 sm:flex-none sm:m-auto bg-dark text-white sm:rounded transition-spacing transition-flex ease-in-out duration-1000 sm:px-8 lg:w-2/5">
+        <section className="relative flex-1">
+          <article className="">
+            <Outlet />
+          </article>
+          <Cat className="w-20 absolute -top-16 right-0" />
+        </section>
       </div>
     </main>
   );
