68c1f266cdbfe4a17aed966774ff47f3e2d7329a
lauravivan <lauvivang@gmail.com>
2024-02-27 10:43:57 -0300

    Update aside navbar position, icons construction and add adjustments to different screen size

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,21 +1,18 @@
 /* eslint-disable react/prop-types */
 import { Outlet, Link } from "react-router-dom";
-import {
-  PersonIcon,
-  FavoriteIcon,
-  LogoutIcon,
-  PersonalPostsIcon,
-} from "icons/Icon";
 import { useAuthContext } from "context/authContext";
 import { Navigate } from "react-router-dom";
 import Input from "UIElements/Input";
 import Search from "UIElements/Search";
 import { getCredentials } from "util/store";
+import IconModel from "@/shared/icons/IconModel";
 
-function AsideItem({ children }) {
+function AsideItem({ href, iconName }) {
   return (
-    <li className="rounded-md text-light hover:bg-light hover:text-dark hover:opacity-85 py-3 cursor-pointer">
-      {children}
+    <li className="rounded-md text-light hover:bg-light hover:text-dark hover:opacity-85 cursor-pointer">
+      <Link to={href}>
+        <IconModel fontSize="30px">{iconName}</IconModel>
+      </Link>
     </li>
   );
 }
@@ -29,74 +26,56 @@ function Root() {
 
   return (
     <div className="h-screen bg-light flex">
-      <div className="w-full h-full md:w-4/5 md:h-12/13 md:rounded-lg m-auto bg-dark flex">
-        <div className="flex h-full w-full md:rounded-lg">
-          <aside className="bg-dark w-20 md:rounded-l-lg border-r border-r-white">
-            <nav className="flex h-full">
-              <ul className="flex flex-1 h-full flex-col text-center py-3 px-2 gap-y-7">
-                <AsideItem>
-                  <Link to={"/trend-tide/profile"}>
-                    <PersonIcon fontSize="30px" />
-                  </Link>
-                </AsideItem>
-                <AsideItem>
+      <div className="flex h-full w-full md:w-[80%] md:h-12/13 md:rounded-lg m-auto bg-dark flex relative">
+        <aside className="flex h-full md:h-12/13 bg-dark w-20 md:rounded-l-lg border-r border-r-white fixed">
+          <nav className="flex flex-1">
+            <ul className="flex flex-1 flex-col text-center my-7 gap-y-10">
+              <AsideItem href="/trend-tide/profile" iconName="person" />
+              <AsideItem
+                href={"/trend-tide/favorite-posts/" + getCredentials().uid}
+                iconName="favorite"
+              />
+              <AsideItem
+                href={"/trend-tide/posts/" + getCredentials().uid}
+                iconName="speaker_notes"
+              />
+              <div className="mt-auto">
+                <button onClick={signOut}>
+                  <AsideItem iconName="logout" />
+                </button>
+              </div>
+            </ul>
+          </nav>
+        </aside>
+        <div className="flex flex-col flex-1 ms-20">
+          <header className="bg-dark text-white md:rounded-tr-lg px-4 py-1">
+            <nav className="px-3 py-4">
+              <ul className="flex flex-col gap-y-3 md:gap-y-0 sm:flex-row sm:justify-between sm:items-center">
+                <li>
+                  <Link className="font-bold text-lg">Posts</Link>
+                </li>
+                <li className="w-full md:w-[50%]">
+                  <form>
+                    <div className="flex bg-light rounded-lg items-center">
+                      <Input placeholder="Search..." className="text-dark" />
+                      <Search className="mr-3" />
+                    </div>
+                  </form>
+                </li>
+                <li>
                   <Link
-                    to={"/trend-tide/favorite-posts/" + getCredentials().uid}
+                    className="rounded-lg block bg-light text-black py-2 px-1 text-center md:px-4"
+                    to={"/trend-tide/new-post"}
                   >
-                    <FavoriteIcon fontSize="30px" />
+                    Create new post
                   </Link>
-                </AsideItem>
-                <AsideItem>
-                  <Link to={"/trend-tide/posts/" + getCredentials().uid}>
-                    <PersonalPostsIcon fontSize="30px" />
-                  </Link>
-                </AsideItem>
-                <div className="mt-auto">
-                  <AsideItem>
-                    <Link>
-                      <button
-                        onClick={() => {
-                          signOut();
-                        }}
-                      >
-                        <LogoutIcon fontSize="30px" />
-                      </button>
-                    </Link>
-                  </AsideItem>
-                </div>
+                </li>
               </ul>
             </nav>
-          </aside>
-          <div className="flex flex-col h-full w-full">
-            <header className="bg-dark text-white md:rounded-tr-lg px-4 py-1">
-              <nav className="px-3 py-4">
-                <ul className="flex flex-col gap-y-3 md:gap-y-0 sm:flex-row sm:justify-between sm:items-center">
-                  <li>
-                    <Link className="font-bold text-lg">Posts</Link>
-                  </li>
-                  <li className="w-[50%]">
-                    <form>
-                      <div className="flex bg-light rounded-lg items-center">
-                        <Input placeholder="Search..." className="text-dark" />
-                        <Search className="mr-3" />
-                      </div>
-                    </form>
-                  </li>
-                  <li>
-                    <Link
-                      className="rounded-lg block bg-light text-black py-2 px-1 text-center md:px-4"
-                      to={"/trend-tide/new-post"}
-                    >
-                      Create new post
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </header>
-            <main className="text-light h-full w-full md:p-10 overflow-y-auto overflow-x-hidden">
-              <Outlet />
-            </main>
-          </div>
+          </header>
+          <main className="text-light md:p-10 overflow-y-auto overflow-x-hidden">
+            <Outlet />
+          </main>
         </div>
       </div>
     </div>
