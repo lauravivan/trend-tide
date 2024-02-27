9bb3a8ae9109d1a7d010575bb4b54c30a1d7d584
lauravivan <lauvivang@gmail.com>
2024-02-27 17:17:11 -0300

    Fixed icons hover

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -6,9 +6,11 @@ import Input from "UIElements/Input";
 import { getCredentials } from "util/store";
 import Icon from "UIElements/Icon";
 
-function AsideItem({ href, iconName }) {
+function AsideItem({ href, iconName, className }) {
   return (
-    <li className="rounded-md text-light hover:bg-light hover:text-dark hover:opacity-85 cursor-pointer">
+    <li
+      className={`rounded-md text-light hover:bg-light hover:text-dark hover:opacity-85 cursor-pointer ${className}`}
+    >
       <Link to={href}>
         <Icon fontSize="30px">{iconName}</Icon>
       </Link>
@@ -28,32 +30,38 @@ function Root() {
       <div className="flex h-full w-full md:w-[80%] md:h-12/13 md:rounded-lg m-auto bg-dark flex relative">
         <aside className="flex h-full md:h-12/13 bg-dark w-20 md:rounded-l-lg border-r border-r-white fixed">
           <nav className="flex flex-1">
-            <ul className="flex flex-1 flex-col text-center my-7 gap-y-10">
-              <AsideItem href="/trend-tide/profile" iconName="person" />
+            <ul className="flex flex-1 text-center flex-col my-5 gap-y-10">
+              <AsideItem
+                href="/trend-tide/profile"
+                iconName="person"
+                className="mx-2 py-2"
+              />
               <AsideItem
                 href={"/trend-tide/favorite-posts/" + getCredentials().uid}
                 iconName="favorite"
+                className="mx-2 py-2"
               />
               <AsideItem
                 href={"/trend-tide/posts/" + getCredentials().uid}
                 iconName="speaker_notes"
+                className="mx-2 py-2"
               />
-              <div className="mt-auto">
-                <button onClick={signOut}>
-                  <AsideItem iconName="logout" />
+              <div className="flex mt-auto">
+                <button className="flex-1 mx-2" onClick={signOut}>
+                  <AsideItem iconName="logout" className="p-2" />
                 </button>
               </div>
             </ul>
           </nav>
         </aside>
         <div className="flex flex-col flex-1 ms-20">
-          <header className="bg-dark text-white md:rounded-tr-lg px-4 py-1">
+          <header className="bg-dark text-white md:rounded-tr-lg md:px-4 py-1">
             <nav className="px-3 py-4">
               <ul className="flex flex-col gap-y-3 md:gap-y-0 sm:flex-row sm:justify-between sm:items-center">
                 <li>
                   <Link className="font-bold text-lg">Posts</Link>
                 </li>
-                <li className="w-full md:w-[50%]">
+                <li className="w-full sm:w-[50%]">
                   <form>
                     <div className="flex bg-light rounded-lg items-center">
                       <Input placeholder="Search..." className="text-dark" />
