d42305cc87d2985d5ccf8a3b35b6f30075b7774b
lauravivan <lauvivang@gmail.com>
2024-03-03 20:38:52 -0300

    Adjust modal height

--- src/shared/UIElements/Modal.jsx
+++ src/shared/UIElements/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ isClosed, children }) => {
   return createPortal(
     <dialog
       open={!isClosed}
-      className="text-white bg-black z-50 absolute inset-0 rounded-lg py-4 px-6 transition-width ease-in-out duration-1000 w-[95%] md:w-[50%]"
+      className="text-white bg-black z-50 absolute inset-0 rounded-lg py-4 px-6 transition-width ease-in-out duration-1000 w-[95%] h-[95%] md:w-[50%]"
     >
       {children}
     </dialog>,
