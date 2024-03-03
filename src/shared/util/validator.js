d50fe27050b50bac0b55223d67c476d6a03213a3
lauravivan <lauvivang@gmail.com>
2024-03-02 22:44:35 -0300

    Add max length validators

--- src/shared/util/validator.js
+++ src/shared/util/validator.js
@@ -10,4 +10,19 @@ const validateUserName = (username) => {
   return /^(?=(?:.*[a-z]){3})[\w\d\S]{5,15}$/i.test(username);
 };
 
-export { validateEmail, validatePassword, validateUserName };
+const USERNAME_MAX_LENGTH = 15;
+const EMAIL_MAX_LENGTH = 8;
+const PASSWORD_MAX_LENGTH = 40;
+const POST_TITLE_MAX_LENGTH = 100;
+const POST_CONTENT_MAX_LENGTH = 1200;
+
+export {
+  validateEmail,
+  validatePassword,
+  validateUserName,
+  USERNAME_MAX_LENGTH,
+  EMAIL_MAX_LENGTH,
+  PASSWORD_MAX_LENGTH,
+  POST_TITLE_MAX_LENGTH,
+  POST_CONTENT_MAX_LENGTH,
+};
